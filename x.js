3a5041fe9c8b24a8fe58b38aa52cc124168b5885
gimbalgondring <102336629+gimbalgondring@users.noreply.github.com>
2022-04-25 21:31:45 +0700

    Create x.js

---
+++ x.js
@@ -0,0 +1,30 @@
+const userAgent = require('user-agents');
+const puppeteer = require('puppeteer');
+(async () => {
+    for(i=0;i<3000;i++){
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent.toString())
+    await page.goto('https://codehs.com/sandbox/id/python-3-Bwiokn')
+    await page.waitForTimeout(2000)
+    await page.mouse.click(580,125,{'button': 'left'})
+    await page.waitForTimeout(1000)
+    await page.mouse.click(580,400,{'button': 'left'})
+    for(y=0;y<450;y++) {
+        await page.keyboard.press('KeyH')
+        await page.waitForTimeout(2000)
+        await page.keyboard.press('KeyS')
+        await page.waitForTimeout(2000)
+        await page.keyboard.press('KeyH')
+        await page.waitForTimeout(2000)
+        console.log(`a${i}${y}`)
+    }
+    await page.waitForTimeout(1000)
+    await page.mouse.click(630,125,{'button': 'left'})
+    await page.waitForTimeout(5000)
+    await browser.close();
+    }
+})();
