1be04456c3a2dba873aef58cf8a9d707a1b2138f
gimbalgondring <102336629+gimbalgondring@users.noreply.github.com>
2022-05-01 07:54:37 +0700

    Update a2.js

--- a2.js
+++ a2.js
@@ -6,10 +6,10 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.setUserAgent(userAgent.toString())
     await page.goto('https://codehs.com/sandbox/id/python-3-igm5qO')
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(15000)
     await page.mouse.click(580,125,{'button': 'left'})
     await page.waitForTimeout(1000)
-    for(y=0;y<45;y++) {
+    for(y=0;y<450;y++) {
         await page.mouse.click(580,400,{'button': 'left'})
         await page.keyboard.press('KeyH')
         await page.waitForTimeout(5000)
